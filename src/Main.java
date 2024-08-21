f0eaf9576e1dac0e488d3d75560227db066a0968
f-Brizola <brizola95@gmail.com>
2024-08-20 22:44:10 -0400

    Atividade01

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main(String[] args) {
+
+    int idade= 25;
+    int idade2= 30;
+    double altura= 1.75;
+    String nome= "Tainã vieira";
+
+
+    boolean estud= true;
+    String estuda = "Estudante";
+    if (estud){
+
+    }
+
+    //operaçoes//
+
+        int somaidade = idade+idade2;
+        double alturamultpl = altura * 2;
+
+        //Apresentação de Resutado//
+
+        System.out.println("Ola, "+nome);
+        System.out.println("Eu Sou um "+estuda);
+        System.out.println("Minha idade é: "+idade);
+        System.out.println("A minha altura é de:"+ altura);
+        System.out.println("A soma das idade é: "+ somaidade);
+        System.out.println( "A multiplicação da sua altura é "+alturamultpl);
+
+
+
+
+
+
+
+
+
+    }
+}
